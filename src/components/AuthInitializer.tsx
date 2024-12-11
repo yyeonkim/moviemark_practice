5aa1fe91aee6840ab50a992fc0536386fd10e3c3
Yongyeon Kim <seyeon.dev@gmail.com>
2024-12-11 20:38:39 +0900

    chore: add prettierrc

--- src/components/AuthInitializer.tsx
+++ src/components/AuthInitializer.tsx
@@ -8,28 +8,32 @@ import { getUserInfo } from "@/api/auth";
 import { authState } from "@/store/auth";
 import { getCookie, removeCookie } from "@/utils/cookie";
 
-export const AuthInitializer = ({ children }: { children: React.ReactNode }) => {
-	const setAuthState = useSetRecoilState(authState);
+export const AuthInitializer = ({
+    children,
+}: {
+    children: React.ReactNode;
+}) => {
+    const setAuthState = useSetRecoilState(authState);
 
-	useEffect(() => {
-		const initializeAuth = async () => {
-			const accessToken = getCookie("accessToken");
+    useEffect(() => {
+        const initializeAuth = async () => {
+            const accessToken = getCookie("accessToken");
 
-			if (accessToken) {
-				try {
-					const userInfo = await getUserInfo(); // 토큰을 이용해 사용자 정보 조회
-					setAuthState({ user: { email: userInfo.email } });
-				} catch {
-					// 토큰이 만료되었거나 유효하지 않은 경우
-					removeCookie("accessToken");
-					removeCookie("refreshToken");
-					setAuthState({ user: null });
-				}
-			}
-		};
+            if (accessToken) {
+                try {
+                    const userInfo = await getUserInfo(); // 토큰을 이용해 사용자 정보 조회
+                    setAuthState({ user: { email: userInfo.email } });
+                } catch {
+                    // 토큰이 만료되었거나 유효하지 않은 경우
+                    removeCookie("accessToken");
+                    removeCookie("refreshToken");
+                    setAuthState({ user: null });
+                }
+            }
+        };
 
-		initializeAuth();
-	}, [setAuthState]);
+        initializeAuth();
+    }, [setAuthState]);
 
-	return <>{children}</>;
+    return <>{children}</>;
 };
