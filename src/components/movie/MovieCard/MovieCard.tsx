2bb0744741b4116d749b834d92618cac60365f65
Yongyeon Kim <seyeon.dev@gmail.com>
2024-12-13 15:37:12 +0900

    feat: navigate movie detail page when click movie card

--- src/components/movie/MovieCard/MovieCard.tsx
+++ src/components/movie/MovieCard/MovieCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { Movie } from "@/types/movie";
 
 import MovieBookmark from "./MovieBookmark";
@@ -6,26 +8,35 @@ import MoviePoster from "./MoviePoster";
 import MovieRating from "./MovieRating";
 import MovieTitle from "./MovieTitle";
 interface MovieCardProps {
-  movie: Movie;
+    movie: Movie;
 }
 
 const MovieCard = ({ movie }: MovieCardProps) => {
-	if (!movie) return null;
+    if (!movie) return null;
 
-	return (
-		<div className="flex flex-col gap-2 p-4 rounded-xl bg-gray-800 hover:bg-gray-700 transition-colors">
-			<MoviePoster
-				src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-				alt={movie.title}
-			/>
-			<div className="relative flex-1 flex flex-col">
-				<MovieRating value={movie.vote_average} size='lg' />
-				<MovieTitle ko_title={movie.korean_title} en_title={movie.title} />
-				<MovieBookmark movieId={movie.id} isBookmarked={movie.is_bookmarked} />
-			</div>
-			<MovieDate date={movie.release_date} />
-		</div>
-	);
+    return (
+        <Link
+            href={`/movies/${movie.id}`}
+            className="flex  flex-col gap-2 p-4 rounded-xl bg-gray-800 hover:bg-gray-700 transition-colors"
+        >
+            <MoviePoster
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+            />
+            <div className="relative flex-1 flex flex-col">
+                <MovieRating value={movie.vote_average} size="lg" />
+                <MovieTitle
+                    ko_title={movie.korean_title}
+                    en_title={movie.title}
+                />
+                <MovieBookmark
+                    movieId={movie.id}
+                    isBookmarked={movie.is_bookmarked}
+                />
+            </div>
+            <MovieDate date={movie.release_date} />
+        </Link>
+    );
 };
 
 export default MovieCard;
