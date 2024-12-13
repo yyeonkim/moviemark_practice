c4f5c8dca9ae9ed3b6e327e3941f891185211c0e
Yongyeon Kim <seyeon.dev@gmail.com>
2024-12-13 16:23:34 +0900

    feat: add movie search input

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,13 +1,25 @@
 import { Suspense, lazy } from "react";
 
+import BaseInput from "@/components/common/Input/BaseInput";
+
 // 동적 임포트를 통한 지연 로딩
 const MovieList = lazy(() => import("@/components/movie/MovieList"));
-const MovieListSkeleton = lazy(() => import("@/components/movie/MovieListSkeleton"));
+const MovieListSkeleton = lazy(
+    () => import("@/components/movie/MovieListSkeleton")
+);
 
 export default function MoviePage() {
-	return (
-		<Suspense fallback={<MovieListSkeleton />}>
-			<MovieList />
-		</Suspense>
-	);
+    return (
+        <Suspense fallback={<MovieListSkeleton />}>
+            {/* 영화 검색 */}
+            <BaseInput
+                name="movieTitle"
+                placeholder="Enter the movie title"
+                containerClassName="mx-5 mb-10"
+                fullWidth={false}
+            />
+
+            <MovieList />
+        </Suspense>
+    );
 }
